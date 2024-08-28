45399b1cedaf854bf498399d3975de94c6ba3eb7
Ran Yehekzel <ran140009g@gmail.com>
2024-08-28 18:46:39 +0300

    initial commit

--- app/src/main/java/com/example/barbershop/server/ServerRequest.java
+++ app/src/main/java/com/example/barbershop/server/ServerRequest.java
@@ -16,7 +16,7 @@ public class ServerRequest
 {
     public static final String ERROR_RESPONSE = "requestError";
     private  Map map;
-    private String url;
+    private String url = "https://ran-y.me/barbershop/commands/";
     private ServerResponseHandle responseHandle; // method that called when get response from the server
 
     public ServerRequest(ServerResponseHandle responseHandle)
@@ -27,7 +27,7 @@ public class ServerRequest
 
     public void firstLogin(String name,String phone,String idToken,Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/add_new_user.php";
+        url += "user/add_new_user.php";
         map.put("name",name);
         map.put("phone",phone);
         map.put("idToken",idToken);
@@ -36,14 +36,14 @@ public class ServerRequest
 
     public void checkIfNewUser(String userMail,Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/check_if_new_user.php";
+        url += "user/check_if_new_user.php";
         map.put("userMail",userMail);
         sendRequest(context);
     }
 
     public void getUserDetails(String userMail,String secretKey,Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/get_user_details.php";
+        url += "user/get_user_details.php";
         map.put("userMail",userMail);
         map.put("secretKey",secretKey);
         sendRequest(context);
@@ -51,14 +51,14 @@ public class ServerRequest
 
     public void checkGoogleLogIn(String idToken, Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/check_google_login.php";
+        url += "user/check_google_login.php";
         map.put("idToken",idToken);
         sendRequest(context);
     }
 
     public void deleteQueue(Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/remove_reserved_queue_and_add_empty_queue.php";
+        url += "user/remove_reserved_queue_and_add_empty_queue.php";
         map.put("userMail",DataHolderClass.userMail);
         map.put("userName", DataHolderClass.userName);
         map.put("secretKey",DataHolderClass.userSecretKey);
@@ -68,7 +68,7 @@ public class ServerRequest
 
     public void logOutFromAllDevices(Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/make_new_uuid.php";
+        url += "user/make_new_uuid.php";
         map.put("userMail",DataHolderClass.userMail);
         map.put("secretKey",DataHolderClass.userSecretKey);
         sendRequest(context);
@@ -76,7 +76,7 @@ public class ServerRequest
 
     public void addQueue(String newDate,Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/add_reserved_queue.php";
+        url += "user/add_reserved_queue.php";
         map.put("userMail",DataHolderClass.userMail);
         map.put("userName",DataHolderClass.userName);
         map.put("secretKey",DataHolderClass.userSecretKey);
@@ -87,7 +87,7 @@ public class ServerRequest
 
     public void getEmptyQueues(Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/ask_for_empty_queues.php";
+        url += "user/ask_for_empty_queues.php";
         map.put("userMail",DataHolderClass.userMail);
         map.put("secretKey",DataHolderClass.userSecretKey);
         sendRequest(context);
@@ -95,7 +95,7 @@ public class ServerRequest
 
     public void updateName(String name,Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/update_name.php";
+        url += "user/update_name.php";
         map.put("userMail",DataHolderClass.userMail);
         map.put("secretKey",DataHolderClass.userSecretKey);
         map.put("name", name);
@@ -104,7 +104,7 @@ public class ServerRequest
 
     public void updatePhone(String phone,Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/update_phone.php";
+        url += "user/update_phone.php";
         map.put("userMail",DataHolderClass.userMail);
         map.put("secretKey",DataHolderClass.userSecretKey);
         map.put("phone", phone);
@@ -113,7 +113,7 @@ public class ServerRequest
 
     public void updateQueue(String newDate,Context context)
     {
-        url = "https://ran-yehezkel.online/barbershop/commands/user/update_queue.php";
+        url += "user/update_queue.php";
         map.put("userMail",DataHolderClass.userMail);
         map.put("userName",DataHolderClass.userName);
         map.put("secretKey",DataHolderClass.userSecretKey);
@@ -123,7 +123,7 @@ public class ServerRequest
 
     public void removeUser(Context context)
     {;
-        url = "https://ran-yehezkel.online/barbershop/commands/user/remove_user.php";
+        url += "user/remove_user.php";
         map.put("userMail",DataHolderClass.userMail);
         map.put("userName",DataHolderClass.userName);
         map.put("secretKey",DataHolderClass.userSecretKey);
